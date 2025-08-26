8564e5b7aaed2c2a937159b6768bd7e1e32cf109
Jeon Seoknam <j20nsuknam@gmail.com>
2025-08-26 10:54:26 +0900

    Initial commit: import race_stack

--- .install_utils/post_create_command.sh
+++ .install_utils/post_create_command.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
+USERNAME=$(whoami)
 # Setup permissions
 # USER="$(id -u -n)"
-sudo chown -R $USERNAME /home/$USERNAME/ws/
+sudo chown -R $USERNAME /home/misys/forza_ws/
 
 # Install dependencies
 rosdep update &&
-    rosdep install --from-paths /home/$USERNAME/ws/src --ignore-src -y
+    rosdep install --from-paths /home/misys/forza_forza_ws --ignore-src -y
 
 # Setup race_stack
-bash ~/ws/src/race_stack/.install_utils/f110_sim_setup.sh || echo "Failed to setup f110_sim"
-bash ~/ws/src/race_stack/.install_utils/gb_opt_setup.sh || echo "Failed to setup gb_opt"
+bash /home/$(whoami)/forza_ws/race_stack/src/race_stack/.install_utils/f110_sim_setup.sh || echo "Failed to setup f110_sim"
+bash /home/$(whoami)/forza_ws/race_stack/src/race_stack/.install_utils/gb_opt_setup.sh || echo "Failed to setup gb_opt"
 
 # Apply Joystick patch
 sudo chmod 666 /dev/input/js0
 sudo chmod 666 /dev/input/event*
 
 # setup f1tenth_gym
-cd ~/ws &&
-    colcon build --packages-up-to f110_gym --base-paths ~/ws \
+cd /home/$(whoami)/forza_ws/race_stack &&
+    colcon build --packages-up-to f110_gym --base-paths /home/$(whoami)/forza_ws/race_stack \
         --cmake-args "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic --cmake-clean-cache
